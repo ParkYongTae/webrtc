82510e82cf5a81e926bb0a76bfc15ec79837a26e
ytpark <pa$$w0rd>
2020-10-27 04:15:12 +0900

    app2.js

--- SignalServer/app2.js
+++ SignalServer/app2.js
@@ -21,21 +21,79 @@ var connectUserListGroupByRoom = [];
 
 io.sockets.on('connection', function(socket){
 
-    // join room
-    socket.on('joinRoom', function(roomId, userName, userImage, isStreamExists){
-        socket.join(roomId);
+    var socketAllList = Object.keys(io.sockets.sockets);
 
-        var joinUserInfo = {
-            socketId : socket.id,
-            roomId : roomId,
-            userName : userName,
-            userImage : userImage,
-            isStreamExists : isStreamExists
-        };
+    // 전체소켓을 조사하여 연결 끊어진 소켓 채팅방에서 제거
+    var roomList = Object.keys(connectUserListGroupByRoom);
 
+    for(var i = 0; i < roomList.length; i++) {
+        if (connectUserListGroupByRoom[roomList[i]]) {
+            for (var k = 0; k < connectUserListGroupByRoom[roomList[i]].length; k++) {
+                var socketId = connectUserListGroupByRoom[roomList[i]][k].socketId;
+
+                var isExists = false;
+                for(var j = 0; j < socketAllList.length; j++){
+                    if(socketAllList[j] == socketId){
+                        isExists = true;
+                    }
+                }
+
+                if (isExists == false) {
+                    connectUserListGroupByRoom[roomList[i]].splice(k, 1);
+                }
+            }
+        }
+    }
+
+    // check video chat list
+    socket.on('checkVideoChatList', function(){
+
+        var roomList = Object.keys(connectUserListGroupByRoom);
+
+        var videoChatList = [];
+
+        for(var i = 0; i < roomList.length; i++) {
+            if (roomList[i] && connectUserListGroupByRoom[roomList[i]].length > 0) {
+                videoChatList.push(roomList[i]);
+            }
+        }
+
+        io.to(socket.id).emit('checkVideoChatListResult', videoChatList);
+    });
+
+    // check room
+    socket.on('checkRoom', function(roomId){
         if(!connectUserListGroupByRoom[roomId]){
             connectUserListGroupByRoom[roomId] = [];
         }
+
+        var checkResult = {
+            type : "SUCCESS"
+        };
+
+        if(connectUserListGroupByRoom[roomId].length >= 10) {
+            checkResult.type = 'FULL'
+        }
+
+        io.to(socket.id).emit('checkRoomResult', checkResult);
+    });
+
+    // join room
+    socket.on('joinRoom', function(roomId, userName, userImage, isStreamExists){
+        if(!connectUserListGroupByRoom[roomId]){
+            connectUserListGroupByRoom[roomId] = [];
+        }
+
+        socket.join(roomId);
+
+        var joinUserInfo = {
+            socketId: socket.id,
+            roomId: roomId,
+            userName: userName,
+            userImage: userImage,
+            isStreamExists: isStreamExists,
+            videoEnabled: true
+        };
 
         connectUserListGroupByRoom[roomId].push(joinUserInfo);
 
@@ -49,7 +107,15 @@ io.sockets.on('connection', function(socket){
         io.to(toId).emit('signal', socket.id, message);
     });
 
-    socket.on('videoToggle', (toId, videoEnabled) => {
+    socket.on('videoToggle', (roomId, toId, videoEnabled) => {
+
+        for (var i = 0; i < connectUserListGroupByRoom[roomId].length; i++) {
+            if (socket.id == connectUserListGroupByRoom[roomId][i].socketId) {
+                connectUserListGroupByRoom[roomId][i].videoEnabled = videoEnabled;
+                break;
+            }
+        }
+
         io.to(toId).emit('videoToggle', socket.id, videoEnabled);
     });
 
@@ -58,7 +124,7 @@ io.sockets.on('connection', function(socket){
         var roomList = Object.keys(connectUserListGroupByRoom);
 
         for(var i = 0; i < roomList.length; i++) {
-            if (roomList[i]) {
+            if (connectUserListGroupByRoom[roomList[i]]) {
                 for (var k = 0; k < connectUserListGroupByRoom[roomList[i]].length; k++) {
                     var socketId = connectUserListGroupByRoom[roomList[i]][k].socketId;
                     if (socketId == socket.id) {
@@ -66,6 +132,9 @@ io.sockets.on('connection', function(socket){
 
                         if (socket.rooms[roomList[i]]) {
                             io.sockets.sockets[socketId].leave(roomList[i]);
+                            socket.disconnect();
+                            socket.close();
+                            break;
                         }
                     }
                 }
